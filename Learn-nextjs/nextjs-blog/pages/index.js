9c319eeee8b70fc9fead0c949782e00bcf8d5b58
Frances He <hesiyun1996@gmail.com>
2021-07-30 19:32:34 +0800

    add: tutorial-> Polishing layout with personal profile

--- Learn-nextjs/nextjs-blog/pages/index.js
+++ Learn-nextjs/nextjs-blog/pages/index.js
@@ -1,7 +1,26 @@
-import Link from 'next/link'
 import Head from 'next/head'
+import Layout, {siteTitle} from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Hello, I'm Siyun. I am software engineer.]</p>
+        <p>  
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
+
+
+/*export default function Home() {
   return (
     <div className="container">
       <Head>
@@ -211,3 +230,4 @@ export default function Home() {
     </div>
   )
 }
+*/
