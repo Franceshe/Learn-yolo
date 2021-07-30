9f3e0d09663fab41835d0ed359d930bfef4b95c7
Frances He <hesiyun1996@gmail.com>
2021-07-30 18:50:12 +0800

    add: metadata title

--- Learn-nextjs/nextjs-blog/pages/posts/first-post.js
+++ Learn-nextjs/nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,12 @@
+import Head from 'next/head' 
 import Link from 'next/link'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
@@ -12,4 +16,5 @@ export default function FirstPost() {
     </>
   )
 }
+
 
